c89c481bd4cf9a7cff6de92e3dad2988e4c30f5e
MatteoT01 <matteo.tonello.3@studenti.unipd.it>
2024-05-01 17:24:09 +0200

    Update matcher_app.cpp

--- src/basic_sfm.cpp
+++ src/basic_sfm.cpp
@@ -824,8 +824,8 @@ bool BasicSfM::incrementalReconstruction( int seed_pair_idx0, int seed_pair_idx1
             R1.copyTo(proj_mat1(cv::Rect(0, 0, 3, 3)));
             translation_vector1.copyTo(proj_mat1(cv::Rect(3, 0, 1, 3)));
             
-            if(checkCheiralityConstraint(new_cam_pose_idx, pt_idx) && checkCheiralityConstraint(cam_idx, pt_idx))
-            {
+            //if(checkCheiralityConstraint(new_cam_pose_idx, pt_idx) && checkCheiralityConstraint(cam_idx, pt_idx))
+            //{
               cv::triangulatePoints(proj_mat0, proj_mat1, points0, points1, hpoints4D);   
               if(hpoints4D.at<double>(2,0)/hpoints4D.at<double>(3,0) > 0.0)
               {
@@ -836,7 +836,7 @@ bool BasicSfM::incrementalReconstruction( int seed_pair_idx0, int seed_pair_idx1
                 pt[1] = hpoints4D.at<double>(1,0)/hpoints4D.at<double>(3,0);
                 pt[2] = hpoints4D.at<double>(2,0)/hpoints4D.at<double>(3,0);
               }
-            }
+            //}
             
             points0.clear();
             points1.clear();
@@ -961,7 +961,7 @@ void BasicSfM::bundleAdjustmentIter( int new_cam_idx )
           observations_[2*i_obs],
           observations_[2*i_obs + 1]);
 
-        LossFunction* loss_function = new ceres::CauchyLoss(2 * max_reproj_err_);
+        ceres::LossFunction* loss_function = new ceres::CauchyLoss(2 * max_reproj_err_);
 
         double* camera = cameraBlockPtr (cam_pose_index_[i_obs]);
         double* point = pointBlockPtr (point_index_[i_obs]);
@@ -969,7 +969,7 @@ void BasicSfM::bundleAdjustmentIter( int new_cam_idx )
         problem.AddResidualBlock(cost_function,
                            loss_function,
                            camera,
-                           point;
+                           point);
         
         
         
